f07fe14b4546b06d5f5a935d448300dcb7c6fac1
xin wei <xin_wei@intuit.com>
2018-09-18 02:14:24 -0700

    add some schema class

---
+++ dse-graphql/src/main/java/com/weixin/query/GraphQLEndpoint.java
@@ -0,0 +1,19 @@
+package com.weixin.query;
+
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(SchemaParser.newParser()
+                .file("schema.graphqls") //parse the schema file created earlier
+                .build()
+                .makeExecutableSchema());
+    }
+}
